8c3183d43b8c5211d79f2fb218d5215f616e70ff
User <leha.gulko.91@mail.ru>
2023-04-01 12:47:31 +0300

    first commit

---
+++ ukrashrenie/WpfApp1/Instances.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Instances
+    {
+        private static DemkaEntities _db;
+
+        public static DemkaEntities db
+        {
+            get
+            {
+                if (_db == null) ;
+                _db = new DemkaEntities();
+                return _db;
+            }
+        }
+    }
+}
---
+++ ukrashrenie/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void enter_button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var user = Instances.db.Пользователь.FirstOrDefault(q => q.логин.Equals(login_text.Text) && q.пароль.Equals(password_text.Password));
+                    if (user != null)
+                {
+                    Window newWindow = new Window1();
+                    this.Close();
+                    newWindow.Show();
+                }
+                else
+                {
+                    MessageBox.Show("NO");
+                }
+                 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "HYET", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+        }
+    }
+}
